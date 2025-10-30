79b815bd5a4ca883491a2143eeb7080cec5334d8
Iván Ruiz <iruizl2004@gmail.com>
2025-10-30 18:23:57 +0100

    feat: Crear el módulo Users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './modules/users/entities/user.entity';
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({ isGlobal: true }),
-    ],
+	imports: [
+		ConfigModule.forRoot({ isGlobal: true }),
+		TypeOrmModule.forRoot({
+			type: 'postgres',
+			host: process.env.POSTGRESQL_HOST,
+			port: parseInt(process.env.POSTGRESQL_PORT || '3306'),
+			username: process.env.POSGRESQL_USERNAME,
+			password: process.env.POSTGRESQL_PASSWORD,
+			database: process.env.POSTGRESQL_DATABASE,
+			entities: [User],
+			synchronize: true,
+		}),
+	],
 })
 export class AppModule {}
