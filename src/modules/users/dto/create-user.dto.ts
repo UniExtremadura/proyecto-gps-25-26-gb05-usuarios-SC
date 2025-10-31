2589f7ce6f9e6b1be3073161e1c57f490e534ddb
Alejandro Paniagua <apaniaguu@alumnos.unex.es>
2025-10-31 19:45:59 +0100

    feat: GB05-35 Validar campos del formulario

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,3 +1,20 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+import { UserRole } from '../entities/user.entity';
+
 export class CreateUserDto {
-	// TODO: Definir CreateUserDto
+	@IsNotEmpty()
+	firstName: string;
+
+	@IsNotEmpty({ message: 'El apellido no puede estar vacío' })
+	lastName: string;
+
+	@IsEmail()
+	@IsNotEmpty({ message: 'El email no puede estar vacío' })
+	email: string;
+
+	@IsNotEmpty({ message: 'La contraseña no puede estar vacío' })
+	password: string;
+
+	@IsNotEmpty({ message: 'El rol no puede estar vacío' })
+	role: UserRole;
 }
